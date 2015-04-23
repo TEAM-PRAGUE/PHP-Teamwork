578457f4c1cde37458b0234fc25e2cfcdc631c42
IvayloVV <alkmaar@abv.bg>
2015-04-23 12:24:50 +0300

    Index, style, design updates.

--- index.php
+++ index.php
@@ -1,12 +1,42 @@
 <html>
 <head>
-    <title>My first PHP Website</title>
+    <title>Spend now Save Never</title>
+    <link rel="stylesheet" href="style.css"/>
+
 </head>
 <body>
-    <?php
-        echo "<p>Hello World!</p>";
-    ?>
-    <a href="login.php"> Click here to login <br/>
-    <a href="register.php"> Click here to register
+   <section id="main">
+       <div id="reg">
+    <a href="login.php"> Click here to login</a> <br/>
+    <a href="register.php"> Click here to register</a>
+       </div>
+
+        <h1>SPEND NOW SAVE NEVER</h1>
+
+       <div id="header">
+           <ul>
+               <li><a href="">HOME</a></li>
+               <li><a href="">ABOUT</a></li>
+               <li><a href="">CONTACT</a></li>
+               <li><a href="">SHIRTS</a></li>
+           </ul>
+       </div>
+
+           <div id="sidebar">
+
+               <a href="">ARCHIVE</a> <br/> <br/>
+               <a href=""> MAY </a> <br/>
+               <a href=""> APRIL </a> <br/>
+               <a href=""> MARCH </a> <br/>
+               <a href=""> FEBRUARY </a> <br/>
+               <a href=""> JANUARY </a> <br/>
+       </div>
+
+       <div id="content">
+
+
+       </div>
+
+   </section>
 </body>
 </html> 
