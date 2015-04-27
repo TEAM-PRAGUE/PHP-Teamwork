093d85d088562242d77eb6baf18c06d89d5c3bc7
IvayloVV <alkmaar@abv.bg>
2015-04-27 18:14:31 +0300

    about page edit

--- about.php
+++ about.php
@@ -27,8 +27,21 @@
     <div id="aboutcontent">
 
         <h2>New money? Start here!</h2>
-        Let me be the first to welcome you in the club of the millionaires! It doesn't matter if you earned your money
-        by buying oil, selling records, or simply becoming a YouTube sensation! Do point is now, like never before
+        <img src="img/abt.jpg" alt="carhouse"/>
+        <br/>
+        <br/>
+
+        Let me be the first to welcome you in the club of the millionaires!
+        If you are reading this page you have probably made your big money the following ways:
+        <br/> Won the lottery
+        <br/> 17 year old football player who just signed for Manchester United
+        <br/> Self appointed CEO of your popular YouTube gaming channel
+        <br/> A twitter personality
+        <br/> Son of a Arabian sheikh
+        <br/> Wrote incredibly dumb rap songs that have 450 million views on YouTube
+
+
+
         YOU have the power to spend money. And don't just spend them! WASTE THEM!
         This is where the SPEND NOW SAVE NEVER &trade; blog comes into play! We will give you the best ideas on
         how to spend those easy earned money like there is no tomorrow!
