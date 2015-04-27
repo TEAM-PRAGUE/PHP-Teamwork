ae5c5c84fa0023c7c7ad9900d423799e8c61f7db
Simeon Stoykov <simeon_stoykov@mail.bg>
2015-04-27 16:28:28 +0300

    Add design

---
+++ contact.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+    <title>Spend now Save Never</title>
+    <link rel="stylesheet" href="style.css"/>
+
+</head>
+<body>
+<section id="main">
+    <div id="reg">
+        <a href="login.php"> Click here to login</a> <br/>
+        <a href="register.php"> Click here to register</a>
+    </div>
+
+    <h1>SPEND NOW SAVE NEVER</h1>
+
+    <div id="header">
+        <ul>
+            <li><a href="index.php">HOME</a></li>
+            <li><a href="about.php">ABOUT</a></li>
+            <li><a href="">CONTACT</a></li>
+            <li><a href="shirts.php">SHIRTS</a></li>
+        </ul>
+    </div>
+
+
+    <div id="contactlist">
+        <img src="img/adress.png" alt="" />
+        <p>Our postal address is: <br/></p>
+        15 Boulevard de Suisse <br/>
+        Monaco-Ville <br/>
+        MC 98000 <br/>
+        Monaco <br/>
+        (Look for the black Maserati)
+
+
+
+    </div>
+
+
+</section>
+</body>
+</html> 
---
+++ shirts.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+    <title>Spend now Save Never</title>
+    <link rel="stylesheet" href="style.css"/>
+
+</head>
+<body>
+<section id="main">
+    <div id="reg">
+        <a href="login.php"> Click here to login</a> <br/>
+        <a href="register.php"> Click here to register</a>
+    </div>
+
+    <h1>SPEND NOW SAVE NEVER</h1>
+
+    <div id="header">
+        <ul>
+            <li><a href="index.php">HOME</a></li>
+            <li><a href="about.php">ABOUT</a></li>
+            <li><a href="contact.php">CONTACT</a></li>
+            <li><a href="shirts.php">SHIRTS</a></li>
+        </ul>
+    </div>
+
+
+    <div id="shirtcontent">
+   <div id="shirts"><img src="img/shirt1.jpg" alt="" width="200px"/> <img src="img/shirt2.jpg" alt="" width="200px"/>
+       <img src="img/shirt3.jpg" alt="" width="200px"/>
+       <img src="img/shirt5.jpg" alt="" width="200px"/>
+       <img src="img/shirt6.jpg" alt="" width="200px"/>
+       <img src="img/shirt7.jpg" alt="" width="200px"/></div>
+
+
+    </div>
+
+</section>
+</body>
+</html> 
